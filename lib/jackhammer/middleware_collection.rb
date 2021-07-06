4a5385631cd16edb3d50db94f838753cf7f97c45
Krzysztof Knapik <knapo@knapo.net>
2021-07-06 15:30:38 +0200

    Support ruby 3.0

--- lib/jackhammer/middleware_collection.rb
+++ lib/jackhammer/middleware_collection.rb
@@ -4,25 +4,25 @@ module Jackhammer
       @entries = []
     end
 
-    def use(klass, *args, &block)
-      @entries << Entry.new(klass: klass, args: args, block: block)
+    def use(klass, *args, **kwargs, &block)
+      @entries << Entry.new(klass: klass, args: args, kwargs: kwargs, block: block)
     end
 
-    def call(*args, &block)
+    def call(*args, **kwargs, &block)
       call_chain = @entries.map(&:instantiate) + [block]
 
-      traverse = proc do |*arguments|
-        call_chain.shift.call(*arguments, &traverse) unless call_chain.empty?
+      traverse = proc do |*procargs, **prockwargs|
+        call_chain.shift.call(*procargs, **prockwargs, &traverse) unless call_chain.empty?
       end
 
-      traverse.call(*args)
+      traverse.call(*args, **kwargs)
     end
 
-    Entry = Struct.new(:klass, :args, :block, keyword_init: true) do
+    Entry = Struct.new(:klass, :args, :kwargs, :block, keyword_init: true) do
       def instantiate
         return klass unless klass.respond_to?(:new)
 
-        klass.new(*args, &block)
+        klass.new(*args, **kwargs, &block)
       end
     end
   end
